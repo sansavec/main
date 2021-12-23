871db982016558e7f853c1ab878178e678fe6fb2
sansavec <sansavec1@gmail.com>
2021-12-23 11:26:34 +0100

    time updated

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-import requests
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 opts = Options()
 opts.add_argument('--headless')
 
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=opts)
+s = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=s, options=opts)
+#driver = webdriver.Chrome(ChromeDriverManager().install(), options=opts)
 driver.get('https://sat.tvmucho.com/app/tvmucho/?free&autostart&subscription=uk')
-    
-jwtoken = driver.execute_script('return window.localStorage["token"]')
-   
-if jwtoken:
-  with open('token', 'w') as f:
-    f.write(jwtoken)
 
-driver.quit()
+jwtoken = driver.execute_script('return window.localStorage["token"]')

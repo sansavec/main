b5aef7b0cb11991c96c1a16396716eb3fa527d3b
sansavec <92172411+sansavec@users.noreply.github.com>
2021-12-23 17:25:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ driver = webdriver.Chrome(service=s, options=opts)
 #driver = webdriver.Chrome(ChromeDriverManager().install(), options=opts)
 driver.get('https://sat.tvmucho.com/app/tvmucho/?free&autostart&subscription=uk')
 
-#jwtoken = driver.execute_script('return window.localStorage["token"]')
-jwtoken = driver.execute_script('return window.localStorage')
+jwtoken = driver.execute_script('return window.localStorage["refresh_token"]')
 print(jwtoken)
 
 if jwtoken:
